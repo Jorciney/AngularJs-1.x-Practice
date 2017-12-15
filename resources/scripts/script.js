15afe5e95e71bc0585ab544cd241b3c047c444bd
Jorciney Dias Chaveiro <jorciney.dias.chaveiro@cipalschaubroeck.be>
2017-12-15 15:20:32 +0100

    added search form using the ng-model directive

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -7,14 +7,14 @@ myApp.controller('MainController', ['$scope', function ($scope) {
         firstName: 'Jorciney',
         lastName: 'Dias Chaveiro',
         imageSrc: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAoGAAAAJDA4N2VmMzkzLTBiNWQtNDU2NS1iZDA1LTc4YjkwYzQwMTJkMQ.jpg'
-    };
+    };	
 
     $scope.message = 'Hello, Angular world!';
     $scope.person = person;
 }]);
 
 myApp.controller('MyControllerTestingGET', ['$scope', '$http', function ($scope, $http) {
-    $scope.title = 'Testing http GET method'
+    $scope.title = 'Testing http GET method';
 
     // Way to get data from html(JSon format)
     var promise = $http.get('https://api.github.com/users/robconery');
@@ -25,16 +25,16 @@ myApp.controller('MyControllerTestingGET', ['$scope', '$http', function ($scope,
     // Second way to get the data
     $http.get('https://api.github.com/users/simonjefford')
         .then(function (response) {
-            $scope.user2 = response.data
+            $scope.user2 = response.data;
         });
 
     //Third way to do get the data
     var onUserComplete = function (response) {
         $scope.user3 = response.data;
-    }
+    };
     var onError = function (reason) {
         $scope.error = 'Could not fetch the user!';
-    }
+    };
     $http.get('https://api.github.com/users/john')
         .then(onUserComplete, onError);
     //note that you can pass a second function in case of error
